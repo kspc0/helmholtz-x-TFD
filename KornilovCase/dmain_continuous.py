fixed continuous derivative

KornilovCase/dmain_continuous.py
@@ -42,16 +42,22 @@ results_dir = "/Results" # folder for saving results
 eigenvalues_dir = "/PlotEigenvalues" # folder for saving eigenvalues
 
 
+#--------------------------MAIN PARAMETERS-------------------------#
+mesh_resolution = 0.01 # specify mesh resolution
+duct_length = 1 # length of the duct
+degree = 3 # the higher the degree, the longer the calulation takes but the more precise it is
+frequ = 80 # where to expect first mode in Hz
+
+
 #--------------------------CREATE MESH----------------------------#
 print("\n--- CREATING MESH ---")
 gmsh.initialize() # start the gmsh session
 gmsh.model.add("KornilovCase") # add the model name
-mesh_resolution = 0.01 # specify mesh resolution
 # locate the points of the 2D geometry: [m]
 p1 = gmsh.model.geo.addPoint(0, 0, 0, mesh_resolution)  
 p2 = gmsh.model.geo.addPoint(0, 0.1, 0, mesh_resolution) # 0.1m high
-p3 = gmsh.model.geo.addPoint(1, 0.1, 0, mesh_resolution) # 1m long
-p4 = gmsh.model.geo.addPoint(1, 0, 0, mesh_resolution)
+p3 = gmsh.model.geo.addPoint(duct_length, 0.1, 0, mesh_resolution) # 1m long
+p4 = gmsh.model.geo.addPoint(duct_length, 0, 0, mesh_resolution)
 # create outlines by connecting points
 l1 = gmsh.model.geo.addLine(p1, p2) # inlet boundary
 l2 = gmsh.model.geo.addLine(p2, p3) # upper wall
@@ -95,7 +101,6 @@ boundary_conditions_hom = {1:  {'Neumann'}, # inlet
                            3:  {'Neumann'}, # upper wall
                            4:  {'Neumann'}} # lower wall
 # set the polynomial degree of the base function of the function space
-degree = 3 # the higher the degree, the longer the calulation takes but the more precise it is
 # define temperature gradient function in geometry
 T = dparams.temperature_step_gauss_plane(mesh, dparams.x_f, dparams.T_in, dparams.T_in, dparams.amplitude, dparams.sig) # the central variable that affects is T_out! if changed to T_in we get the correct homogeneous starting case
 # calculate the sound speed function from temperature
@@ -123,7 +128,6 @@ D = DistributedFlameMatrix(mesh, w, h, rho, T, dparams.q_0, dparams.u_b, FTF, de
 print("\n--- STARTING NEWTON METHOD ---")
 # set the target (expected angular frequency of the system)
 # unit of target: ([Hz])*2*pi = [rad/s] 
-frequ = 55 # 6000 Hz
 target = (frequ)*2*np.pi # 6000 Hz
 # LRF:   GrowthRate + Frequ*j                   Re(w) + Im(w)
 # HelmX: Frequ + GrowthRate*j                   Im(w) - Re(w)
@@ -159,28 +163,10 @@ omega_dict = {'direct':omega_dir, 'adjoint': omega_adj}
 # save as textfile
 dict_writer(path+results_dir+"/eigenvalues", omega_dict)
 # save direct and adjoint eigenvector solution as xdmf files
-xdmf_writer(path+results_dir+"/p_dir", mesh, p_dir) # as xdmf file for paraview analysis
+xdmf_writer(path+results_dir+"/p_dir", mesh, p_dir) # as xdmf file 1.747/2/pifor paraview analysis
 xdmf_writer(path+results_dir+"/p_dir_abs", mesh, absolute(p_dir)) # also save the absolute pressure distribution
 xdmf_writer(path+results_dir+"/p_adj", mesh, p_adj)
 xdmf_writer(path+results_dir+"/p_adj_abs", mesh, absolute(p_adj))
-
-
-# print("\n--- CALCULATING LAPLACIAN OF p_dir ---")
-# # Define function space for Laplacian calculation
-# V = FunctionSpace(mesh, ("CG", degree))
-# # Define the Laplacian operator
-# expre = div(grad(p_dir))
-# # Project the Laplacian onto the function space
-# laplacian_p_dir = Function(V)
-# # Variational problem
-# u = TrialFunction(V)
-# v = TestFunction(V)
-# a = inner(u, v) * dx
-# L = inner(expre, v) * dx
-# # Solve projection
-# problem = LinearProblem(a, L, u=laplacian_p_dir)
-# laplacian = problem.solve()
-# xdmf_writer(path+results_dir+"/laplacian_p_dir", mesh, laplacian)
 
 
 #-------------------CALCULATE SHAPE DERIVATIVES-------------------#
@@ -211,7 +197,8 @@ derivatives_outlet = ShapeDerivativesFFDRectFullBorder(Kornilov, physical_facet_
 print("\n")
 print(f"---> \033[1mTarget =\033[0m {frequ} Hz")
 print(f"---> \033[1mEigenfrequency =\033[0m {round(omega_dir.real/2/np.pi)} + {round(target.imag/2/np.pi)}j Hz")
-print(f"---> \033[1mShape Derivative =\033[0m {derivatives_inlet} {derivatives_outlet}")
+print(f"---> \033[1mShape Derivative =\033[0m {round(derivatives_inlet[1].real/2/np.pi,6)} + {round(derivatives_inlet[1].imag/2/np.pi,6)}j")
+print(f"---> \033[1mShape Derivative =\033[0m {round(derivatives_outlet[1].real/2/np.pi,6)} + {round(derivatives_outlet[1].imag/2/np.pi,6)}j")
 # close the gmsh session which was required to run for calculating shape derivatives
 gmsh.finalize()
 # mark the processing time:

helmholtz_x/eigenvectors.py
@@ -55,7 +55,7 @@ def normalize_eigenvector(mesh, obj, i, absolute=False, degree=1, which='right',
     meas = np.sqrt(mesh.comm.allreduce(assemble_scalar(form(p*p*dx)), op=MPI.SUM))
     # only print normalization measure for right eigenvector
     if which == 'right':
-        print("- measure of normalization for eigenvector: m=", meas)
+        print("- measure of normalization for eigenvector: m=", round(meas.real,6))
 
     temp = vr.array
     # normalize by dividing array of eigenvector by the measure
@@ -168,6 +168,7 @@ def normalize_adjoint(omega_dir, p_dir, p_adj, matrices, D=None):
     if not B and not D:
         # + 2 \omega C
         dL_domega = matrices.C * (2 * omega_dir)
+        print("- using normalization 2 omega C")
     elif B and not D:
         # B + 2 \omega C
         dL_domega = (B +
@@ -175,7 +176,8 @@ def normalize_adjoint(omega_dir, p_dir, p_adj, matrices, D=None):
     elif D and not B:
         # 2 \omega C - D'(\omega)
         dL_domega = (matrices.C * (2 * omega_dir) -
-                     D.get_derivative(omega_dir))
+                    D.get_derivative(omega_dir))
+        print("- using normalization 2 omega C - D'")
     else:
         # B + 2 \omega C - D'(\omega)
         dL_domega = (B +
@@ -187,7 +189,7 @@ def normalize_adjoint(omega_dir, p_dir, p_adj, matrices, D=None):
     print("- measure of the shape derivative normalization: ", meas)
     p_adj_vec = multiply(p_adj_vec, 1 / meas)
 
-    p_adj1 = p_adj  # problem of same nameK of object?? maybe change to p_adj1 = p_adj.copy()?
+    p_adj1 = p_adj.copy()  # problem of same nameK of object?? maybe change to p_adj1 = p_adj.copy()?
     p_adj1.name = "p_adj"
     p_adj1.vector.setArray(p_adj_vec.getArray())
     p_adj1.x.scatter_forward()

helmholtz_x/shape_derivatives.py
@@ -231,7 +231,7 @@ def ShapeDerivativesFFDRectFullBorder(geometry, physical_facet_tag, norm_vector,
     # print("type of Gradient object: ",type(G_neu))  
 
     derivatives = {}
-    print("- shape derivatives of border")
+    print("- assembling shape derivatives of border")
     # calculate the local diplacement field V at the border
     V_ffd = ffd_displacement_vector_rect_full_border(geometry, physical_facet_tag, norm_vector, deg=1)
     # calculate the shape derivative of the control point
@@ -240,9 +240,9 @@ def ShapeDerivativesFFDRectFullBorder(geometry, physical_facet_tag, norm_vector,
     #print(inner(V_ffd, normal))
     #print("V_ffd values:", len(V_ffd.vector.array))
     # Integrate inner(V_ffd, normal) over the domain
-    #V_ffd_normal_form = form(inner(V_ffd, normal) * ds(physical_facet_tag))
-    #V_ffd_normal_value = assemble_scalar(V_ffd_normal_form)
-    # print("Integral of inner(V_ffd, normal) over the domain:", V_ffd_normal_value)
+    V_ffd_normal_form = form(inner(V_ffd, normal) * ds(physical_facet_tag))
+    V_ffd_normal_value = assemble_scalar(V_ffd_normal_form)
+    #print("Integral of inner(V_ffd, normal) over the domain:", V_ffd_normal_value)
 
     # calculate the shape derivative of the border
     shape_derivative_form = form(inner(V_ffd, normal) * G_neu * ds(physical_facet_tag))
@@ -283,13 +283,14 @@ def ffd_displacement_vector_rect_full_border(geometry, surface_physical_tag, nor
     dofs_Q = dofs_Q.reshape(-1,2)
 
     number_of_mesh_points_on_border = round(len(coords)/3) # for 3 dimensions
+    print("- number of mesh points on border:", number_of_mesh_points_on_border)
     # list of values containing each node as element, calculated by the bernstein polynomials
     # value = comb(FFDLattice.l-1,i)*np.power(1-s, FFDLattice.l-1-i)*np.power(s,i) * \
     #         comb(FFDLattice.m-1,j)*np.power(1-t, FFDLattice.m-1-j)*np.power(t,j)
     # equally distribute displacement vector on all mesh points to total sum up to 1
     value = np.zeros(number_of_mesh_points_on_border) # number of mesh points on the border
     for element in range(0,number_of_mesh_points_on_border):
-        value[element] = 1/number_of_mesh_points_on_border # value in total should sum up to 1
+        value[element] = 1#/number_of_mesh_points_on_border # value in total should sum up to 1
     #print("value", value)
     print("- integral of displacment field mesh vectors =", np.sum(value)) # should be =1 ?ß
 