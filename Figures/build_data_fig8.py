fixed sign error to stay consistent in HelmX approach for laplace transformation

Figures/build_data_fig8.py
@@ -32,14 +32,14 @@ for perturbation in perturbations:
     KornilovCase.perturb_kornilov_mesh("x")
     KornilovCase.calculate_discrete_derivative()
     # save the calculated shape derivative
-    discrete_shape_derivatives.append(KornilovCase.derivative/-2/np.pi*KornilovCase.perturbation)
+    discrete_shape_derivatives.append(KornilovCase.derivative/2/np.pi*KornilovCase.perturbation)
 gmsh.finalize() # close the gmsh session
 
 # extract the discrete shape derivatives as complex numbers
 real_discrete_shape_derivatives = [derivative.real for derivative in discrete_shape_derivatives]
 imag_discrete_shape_derivatives = [derivative.imag for derivative in discrete_shape_derivatives]
 # Save the real and imaginary derivatives along with the perturbations to a text file
-output_file = os.path.join(path, 'data_fig12.txt')
+output_file = os.path.join(path, 'data_fig8.txt')
 with open(output_file, 'w') as f:
     f.write("Perturbation [m], delta omega Real Part [Hz/m], delta omega Imaginary Part [Hz/m] \n")
     for p, real, imag in zip(perturbations, real_discrete_shape_derivatives, imag_discrete_shape_derivatives):

test_case.py
@@ -435,7 +435,7 @@ class TestCase:
         logging.info(f"---> \033[1mPerturbation Distance =\033[0m {self.perturbation} m")
         logging.info(f"---> \033[1mTarget =\033[0m {self.target} Hz ")
         logging.info(f"---> \033[1mOmega =\033[0m {self.omega_dir.real} + {self.omega_dir.imag}j ({stability})")
-        logging.info(f"---> \033[1m(Physical Frequency) =\033[0m {self.omega_dir.real/2/np.pi} Hz") # needs negative sign to fit the physical frequency
+        logging.info(f"---> \033[1m(Physical Frequency) =\033[0m {self.omega_dir.real/2/np.pi} Hz")
         logging.info(f"---> \033[1m(Physical Growth Rate) =\033[0m {self.omega_dir.imag/2/np.pi} 1/s")
         #logging.info(f"---> \033[1mEigenfrequency =\033[0m {round(self.omega_dir.real/2/np.pi,4)} + {round(self.omega_dir.imag/2/np.pi,4)}j Hz ({stability})")
         logging.info(f"---> \033[1m{self.type.capitalize()} Shape Derivative =\033[0m {round(self.derivative.real/2/np.pi,8)} + {round(self.derivative.imag/2/np.pi,8)}j")